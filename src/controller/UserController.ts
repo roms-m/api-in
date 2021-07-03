8a00736d8debe566bee61e1415dd185d2b6411c1
Romario Mojica <roms.macmac@gmail.com>
2021-07-02 20:43:07 -0500

    Se crearon los mètodos para el usuario

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,124 @@
 import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
+import { Request, Response} from "express";
 import {User} from "../entity/User";
+const bcrypt = require('bcrypt');
 
 export class UserController {
 
-    private userRepository = getRepository(User);
+    static getAllUsers = async (req: Request, res: Response) => {
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
-    }
+        let users;
+        const userRepository = getRepository(User);
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
-    }
+        try {
+            
+            users = await userRepository.find();
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
-    }
+        } catch (error) {
+            
+            res.status(404).json({
+                message: "Algo salio mal"
+            });
+        }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+        if(users.length > 0){
+
+            res.send(users);
+        }else{
+            res.status(404).json({
+
+                message: "No se encontraron usuarios"
+            });
+        }
     }
+    
+   static getUserById = async( req: Request, res: Response) => {
+
+    let { id } = req.params;
+    const userRepository = getRepository(User);
+
+    try {
+        
+        const user = await userRepository.findOneOrFail(id);
+        
+        res.send(user);
+
+    } catch (error) {
+        
+        res.status(404).json({
+            message: "No se encontraron resultados"
+        })
+    }
+   }
+
+   static editUser = async( req: Request, res: Response) => {
+        
+        const { id } = req.params;
+        const { nombre, direccion, telefono, email, username, clave } = req.body;
+        let user;
+
+        const userRepository = getRepository(User);
+
+        try {
+            
+            const salt = await bcrypt.genSalt(10);
+            
+            user = await userRepository.findOneOrFail(id);
+
+            user.nombre = nombre;
+            user.direccion = direccion;
+            user.telefono = telefono;
+            user.email = email;
+            user.username = username;
+            user.clave = await bcrypt.hash(clave, salt);
+
+            
+        } catch (error) {
+            
+            res.status(400).json({
+                messaje: "Usuario no encontrado"
+            })
+        }
+
+        try {
+            
+            await userRepository.save(user);
+
+        } catch (error) {
+            
+            res.json(error);
+        }
+
+        res.status(201).json({
+            message: "Usuario actualizado"
+        })
+   }
+
+   static deleteUser = async( req: Request, res: Response) => {
+
+        const { id } = req.params;
+        const userRepository = getRepository(User);
+        let user: User;
+
+        try {
+            
+            user = await userRepository.findOneOrFail(id)
+
+        } catch (error) {
+            
+            res.status(404).json({
+                message: "Usuario no encontrado"
+            });
+        }
+
+        userRepository.delete(id);
+
+        return res.status(201).json({
+        
+            message: "Usuario Eliminado"
+        });
+   }
 
 }
+
+export default UserController;
