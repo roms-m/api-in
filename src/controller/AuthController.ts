ad71079dfc0996429b27ac8988d1655ef130f552
Romario Mojica <roms.macmac@gmail.com>
2021-07-02 20:42:33 -0500

    Se crearon los mètodos login y register

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,9 +1,35 @@
 import { Request, Response} from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
-const bcript = require("bcript");
+const bcript = require("bcrypt");
 
 class AuthController{
+
+    static register = async (req: Request, res: Response) =>{
+        const { nombre, direccion, telefono, email, username, clave } = req.body;
+        const user = new User();
+        
+        try {
+            const salt = await bcript.genSalt(10);
+            const hashedPass = await bcript.hash(clave, salt);
+            const userRepository = getRepository(User);
+            
+            user.nombre = nombre;
+            user.direccion = direccion;
+            user.telefono = telefono;
+            user.email = email;
+            user.username = username;
+            user.clave = hashedPass;
+
+            const newUser = await userRepository.save(user);
+
+            res.status(200).json(newUser);
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+
+
+    }
 
     static login = async (req: Request, res: Response) => {
 
@@ -35,9 +61,12 @@ class AuthController{
 
             !validated && res.status(400).json("Datos incorrectos");
             
+            res.status(200).json(user);
         } catch (error) {
             
         }
 
     }
 }
+
+export default AuthController;
