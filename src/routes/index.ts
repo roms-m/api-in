986025d9c4e90a938683b3f734008ff5e2124c45
Romario Mojica <roms.macmac@gmail.com>
2021-07-03 18:01:08 -0500

    Se agregaron rutas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import auth from './auth';
 import user from './usuario';
 import articulo from './articulo';
+import persona from './persona';
+import ingreso from './ingreso';
+import venta from './venta';
 
 const routes = Router();
 
@@ -10,5 +13,11 @@ routes.use('/auth', auth);
 routes.use('/users', user);
 
 routes.use('/articulo', articulo);
+
+routes.use('/persona', persona);
+
+routes.use('/ingreso', ingreso);
+
+routes.use('/venta', venta);
 
 export default routes;
