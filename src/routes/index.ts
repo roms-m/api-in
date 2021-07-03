3fc3117e063b708fbeefa499ff8c06c76a04f706
Romario Mojica <roms.macmac@gmail.com>
2021-07-02 21:41:33 -0500

    Se agregò la ruta  articulo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import auth from './auth';
-import user from './usuario'
+import user from './usuario';
+import articulo from './articulo';
+
 const routes = Router();
 
 routes.use('/auth', auth);
 
 routes.use('/users', user);
+
+routes.use('/articulo', articulo);
 
 export default routes;
